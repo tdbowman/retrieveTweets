1bb7d8ad9f7bd76a2df68a63cc06ea6cf1f8ddfa
Tim Bowman <414640+tdbowman@users.noreply.github.com>
2019-02-18 10:07:06 -0500

    Add files via upload

---
+++ twitteroauth/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'DkURzHZLglhUiRgItBScw7WPa');
+define('CONSUMER_SECRET', 'tBOSkOrb7C5NE3VlBjWzb4IwBFgyMWCZzbdQPTX8EFMhXjNGMH');
+?>
---
+++ twitteroauth/twitteroauth-master/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'uFTFKCfmUpkuQOW4p26v2Q');
+define('CONSUMER_SECRET', '7NSiELHw9dTZcBrXLX4prXf4Pj7Mxay0woslBSagtI');
+define('OAUTH_CALLBACK', 'http://ella.ils.indiana.edu/~tdbowman/scrapingTools/tweetingSurveyLink/twitteroauth/twitteroauth-master/callback.php');
