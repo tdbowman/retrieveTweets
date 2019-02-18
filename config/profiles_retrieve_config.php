d1ece9487d8656a1322cfa47a9a4dd7e2309d20c
Tim Bowman <414640+tdbowman@users.noreply.github.com>
2019-02-18 10:16:17 -0500

    Delete profiles_retrieve_config.php

--- config/profiles_retrieve_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// CHANGE HERE
-$schema = "db_schema"; # CHANGEME -> database schema
-$table = "db_table"; # CHANGEME -> database table
-
-$dir = "data"; # file directory to store JSON
-
-?>
